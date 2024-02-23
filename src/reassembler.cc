d7e1d152e433a14ef14f967c7ccb601e6d0b91f8
honndaj <lizeqing0@gmail.com>
2024-02-23 21:23:42 +0800

    feat(*): back to checkpoint1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,78 +5,7 @@ using namespace std;
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
 {
   // Your code here.
-  auto len = data.size();
-  if(is_last_substring) {
-    is_receive_last_ = true;
-  }
-  if(output.is_closed()) {
-    return ;
-  }
-  if(first_index + len <= next_byte_){
-    check_close(output);
-  }
-  if(first_index == next_byte_) {// data刚好是要的
-    insert_one_substring(data, output);
-  }
-  else if(first_index < next_byte_) {// data冗余了前面一部分
-    if(first_index + len > next_byte_){
-      std::string t = data.substr(next_byte_ - first_index);//注意不能合并成一行，不能引用非左值（临时变量）
-      insert_one_substring(t, output);
-    }
-  }
-  else {// 加入缓存
-    if(next_byte_ + output.available_capacity() <= first_index) {// 超过当前窗口就不要了
-      return ;
-    } 
-    if(buf_.empty()) {
-      buf_[first_index] = data;
-      temp_bytes_ += data.size();
-      return ;
-    }
-    //若buf非空
-    auto it = buf_.upper_bound(first_index);
-    uint64_t it_index;
-    std::string it_data;
-    if(it != buf_.begin()) {// 有前一块
-      it--;
-      //处理first_index的前一块
-      it_index = static_cast<uint64_t> (it->first);
-      it_data = it->second;
-
-      if(it_index + it_data.size() >= first_index) {
-        if(first_index + len < it_index + it_data.size()) {// 如果被覆盖了
-          return ;
-        }
-        data = it_data.substr(0, first_index - it_index) + data;
-        first_index = it_index;
-        temp_bytes_ -= it_data.size();
-        it = buf_.erase(it);
-      }
-      else {
-        it++;
-      }
-    }
-
-    it_index = static_cast<uint64_t> (it->first);
-    it_data = it->second;
-    for(; it != buf_.end() && it_index < first_index + len;) {// 若有重叠就继续循环， 直到变成一块
-      it_index = static_cast<uint64_t> (it->first);
-      it_data = it->second;
-
-      if(it_index + it_data.size() <= first_index + len) { //被覆盖
-        temp_bytes_ -= it_data.size();
-        it = buf_.erase(it);
-        continue;
-      }
-      //合并块
-      uint64_t overlap_len = first_index + len - it_index;
-      data += it_data.substr(overlap_len);
-      temp_bytes_ -= it_data.size();
-      buf_.erase(it);
-    }
-    buf_[first_index] = data;
-    temp_bytes_ += data.size();
-  }
+ 
 }
 
 uint64_t Reassembler::bytes_pending() const
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -8,42 +8,6 @@
 
 class Reassembler
 {
-private:
-  void insert_one_substring(std::string& data, Writer& output) // 暂存的子流只会在插入子流时发挥作用
-  {
-    push_to_ByteStream(output, data);
-    uint64_t first_buf_byte;
-    // 在从buf里取已收到的子流时，多余的部分是否丢弃？ 现在是按照丢弃来处理
-    while(!buf_.empty() && (first_buf_byte = get_first_buf_byte()) <= next_byte_) {//如果缓存不空且第一个缓存的index小于等于需要的下一个字节
-      if(first_buf_byte + buf_.begin()->second.size() > next_byte_) {//如果缓存有用
-        std::string t = buf_.begin()->second.substr(next_byte_ - first_buf_byte);// 那就把有用部分塞进去
-        push_to_ByteStream(output, t);
-      } 
-      temp_bytes_ -= buf_.begin()->second.size();// 从缓存中移除子流后，需要把缓存的字节数给剪掉
-      buf_.erase(buf_.begin());
-    }
-    check_close(output);
-    //std::cout<< is_receive_last_ << " data is inserting: " << data << std::endl;
-  }
-
-  void push_to_ByteStream(Writer& output, std::string& data) {
-    uint64_t actual_len = std::min(output.available_capacity(), data.size());
-    output.push(data.substr(0, actual_len));
-    next_byte_ += actual_len;
-  }
-
-  uint64_t get_first_buf_byte() {
-    uint64_t first_buf_byte = static_cast<uint64_t>(buf_.begin()->first);
-    return first_buf_byte;
-  }
-
-  void check_close(Writer& output) {
-    if(is_receive_last_ && buf_.empty()) {
-      output.close();
-    }
-  }
-
-
 protected:
   std::map<int, std::string> buf_{};
   uint64_t temp_bytes_ {0};

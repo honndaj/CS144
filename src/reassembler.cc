696fd2e345622b5a1523f6c214ed0b2e20bfe07e
honndaj <lizeqing0@gmail.com>
2024-02-23 21:20:24 +0800

    feat(*): too ugly and too much code. stop at the reassembler_win

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -24,21 +24,58 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
       insert_one_substring(t, output);
     }
   }
-  else {
-    if(next_byte_ + output.available_capacity() <= first_index) {
+  else {// 加入缓存
+    if(next_byte_ + output.available_capacity() <= first_index) {// 超过当前窗口就不要了
       return ;
     } 
-    auto it = buf_.find(first_index);
-    if(it != buf_.end()) {// exist就选长的
-      if(it->second.size() < len) {
-        buf_[first_index] = data;
-        temp_bytes_ += len - buf_[first_index].size();
+    if(buf_.empty()) {
+      buf_[first_index] = data;
+      temp_bytes_ += data.size();
+      return ;
+    }
+    //若buf非空
+    auto it = buf_.upper_bound(first_index);
+    uint64_t it_index;
+    std::string it_data;
+    if(it != buf_.begin()) {// 有前一块
+      it--;
+      //处理first_index的前一块
+      it_index = static_cast<uint64_t> (it->first);
+      it_data = it->second;
+
+      if(it_index + it_data.size() >= first_index) {
+        if(first_index + len < it_index + it_data.size()) {// 如果被覆盖了
+          return ;
+        }
+        data = it_data.substr(0, first_index - it_index) + data;
+        first_index = it_index;
+        temp_bytes_ -= it_data.size();
+        it = buf_.erase(it);
+      }
+      else {
+        it++;
       }
     }
-    else{// not exist就直接放
-      buf_[first_index] = data;
-      temp_bytes_ += len;
+
+    it_index = static_cast<uint64_t> (it->first);
+    it_data = it->second;
+    for(; it != buf_.end() && it_index < first_index + len;) {// 若有重叠就继续循环， 直到变成一块
+      it_index = static_cast<uint64_t> (it->first);
+      it_data = it->second;
+
+      if(it_index + it_data.size() <= first_index + len) { //被覆盖
+        temp_bytes_ -= it_data.size();
+        it = buf_.erase(it);
+        continue;
+      }
+      //合并块
+      uint64_t overlap_len = first_index + len - it_index;
+      data += it_data.substr(overlap_len);
+      temp_bytes_ -= it_data.size();
+      buf_.erase(it);
     }
+    buf_[first_index] = data;
+    temp_bytes_ += data.size();
   }
 }
 

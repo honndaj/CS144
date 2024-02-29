b531d934679e13e7ab5a580d88676428ba6c5bff
honndaj <lizeqing0@gmail.com>
2024-02-29 18:54:41 +0800

    feat(wrapping_integers.*): finish wrap32 Class at checkpoint 2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -2,18 +2,27 @@
 
 using namespace std;
 
+const uint64_t P = 1LL << 32;
+
+uint64_t abs( uint64_t a, uint64_t b )
+{
+  return a > b ? a - b : b - a;
+}
+
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32( uint32_t( ( zero_point.raw_value_ + n ) % P ) );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t delta, k, a, b;
+  delta = raw_value_ < zero_point.raw_value_ ? P + raw_value_ : raw_value_;
+  delta -= zero_point.raw_value_;
+  k = ( checkpoint - delta ) / P;
+  a = k * P + delta;
+  b = ( k + 1 ) * P + delta;
+  return abs( a, checkpoint ) < abs( b, checkpoint ) ? a : b;
 }
--- src/wrapping_integers.hh
+++ src/wrapping_integers.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 
 /*
  * The Wrap32 type represents a 32-bit unsigned integer that:

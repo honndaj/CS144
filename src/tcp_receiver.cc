9e455460bc390878cdb261c7fe89e5cc8e29baf7
honndaj <lizeqing0@gmail.com>
2024-03-01 14:35:54 +0800

    feat(tcp_receiver.*): finished the receiver and finished checkpoint 2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,47 @@
 #include "tcp_receiver.hh"
+#include <cassert>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
   // Your code here.
-  (void)message;
-  (void)reassembler;
-  (void)inbound_stream;
+  if ( message.SYN ) {
+    zero_point_ = message.seqno;
+    is_connecting_ = true;
+    is_connected_ = true;
+  }
+  if ( is_connecting_ ) {
+    uint64_t ab_first_index
+      = message.seqno.unwrap( zero_point_, ackno_ ) + message.SYN; // 计算出绝对index（为啥SYN也可以带数据啊？
+    if ( !message.payload.empty()
+         && ab_first_index != 0 ) { // ab_first_index!=0 防一手异常数据（seqno=0的但SYN不等于1的数据segment）
+      reassembler.insert(
+        ab_first_index - 1, message.payload, false, inbound_stream ); // 字节流的关闭不通过reassemler来实现
+    }
+    ackno_ = inbound_stream.bytes_pushed() + 1; // plus 1 due to the SYN
+    if ( message.FIN ) {
+      last_byte_ = ab_first_index - 1 + message.payload.size();
+      is_receive_FIN_ = true;
+    }
+  }
+  if ( last_byte_ == inbound_stream.bytes_pushed() && is_receive_FIN_ ) { // 如果读完全部字节，并且收到了FIN
+    is_connecting_ = false;
+    ackno_++;
+    inbound_stream.close();
+  }
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   // Your code here.
-  (void)inbound_stream;
-  return {};
+  std::optional<Wrap32> ackno = std::nullopt;
+  uint16_t rwnd = UINT16_MAX;
+  if ( is_connected_ ) { //
+    ackno.emplace( Wrap32::wrap( ackno_, zero_point_ ) );
+  }
+  rwnd = uint16_t(
+    min( uint64_t( rwnd ),
+         inbound_stream.available_capacity() ) ); // note the difference of the type(window_size , capacity)
+  return TCPReceiverMessage( ackno, rwnd );
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -3,9 +3,18 @@
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
 
 class TCPReceiver
 {
+protected:
+  Wrap32 zero_point_ { 0 };
+  uint64_t ackno_ { 0 };
+  uint64_t last_byte_ { 0 };      //通过带FIN标志的segment计算出最后一个字节，来判断是否结束
+  bool is_connecting_ { false };  // 是否正处于连接中
+  bool is_connected_ { false };   // 是否已经连接过
+  bool is_receive_FIN_ { false }; // 是否接收到过FIN
+
 public:
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
